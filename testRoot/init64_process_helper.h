7ab9f55859ca9f12e471782ee0ebf85c4243ecfa
unkn0wName <unkn0wName@gmail.com>
2023-04-05 00:22:04 +0800

    fix: run init error

--- testRoot/init64_process_helper.h
+++ testRoot/init64_process_helper.h
@@ -65,8 +65,32 @@ static std::string safe_run_init64_cmd_wrapper(
 	const char* str_root_key,
 	const char *cmd,
 	ssize_t & out_err) {
-	pid_t target_pid = 1;
-	return safe_inject_process64_run_cmd_wrapper(str_root_key, target_pid,
-												 cmd, out_err);
+	std::string str_cmd_result;
+	if (cmd == NULL || strlen(cmd) == 0) { return {}; }
+
+	fork_pipe_info finfo;
+	if (fork_pipe_child_process(finfo)) {
+		out_err = 0;
+		str_cmd_result = run_init64_cmd_wrapper(
+			str_root_key,
+			cmd,
+			out_err);
+		write_errcode_to_father(finfo, out_err);
+		write_string_to_father(finfo, str_cmd_result);
+		_exit(0);
+		return {};
+	}
+
+	out_err = 0;
+	if (!wait_fork_child_process(finfo)) {
+		out_err = -64001;
+	} else {
+		if (!read_errcode_from_child(finfo, out_err)) {
+			out_err = -64002;
+		} else if (!read_string_from_child(finfo, str_cmd_result)) {
+			out_err = -64003;
+		}
+	}
+	return str_cmd_result;
 }
 #endif /* INIT64_HELPER_H_ */
